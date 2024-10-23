dee52ba6283dd7b631db5aa9f773591e043c5cd8
Accurate-codes <ogidanrighteous13@gmail.com>
2024-10-23 12:15:51 +0100

    first commit

---
+++ apps.js
@@ -0,0 +1,77 @@
+//alert("Thanks for your input");
+//var name = "Mark";//
+
+//alert(2 + 2);
+    
+//var thanx = "Thanks for your input!"
+//alert(thanx);
+//var weight = 150;
+//alert(weight);
+
+
+//alert(144);
+
+//var userName = "mark";
+//alert(userName)
+
+//var name = "Mark";
+//username = "Ace";
+//alert(username);
+
+
+//var number = "Alert";
+//alert(number);
+
+
+//var num = 10;
+//var anotherNum = 1;
+//var popularNumber = num + anotherNum
+//alert(popularNumber);
+
+//var whatsLeftOver = 9 / 3;
+//alert(whatsLeftOver);
+
+
+//var message = "Thanks, ";
+//var banger = "!";
+//var userName = "Emmanuel"   
+//alert(message + userName + banger);
+
+//var whatDoYouWant = prompt("Would you like to Continue?", "YES")
+
+//var question = "where do you live"
+//var defaultAnswer = "lagos"
+//var spec = prompt(question, defaultAnswer)
+
+//var numberOfCats = prompt("How many cats?");
+//var tooManyCats = numberOfCats;
+
+//var where = prompt("Where does the Pope live?");
+//if (where === "vatican") {
+//    alert("Correct!");
+//}
+
+//else {
+  //  alert("Wrong answer");
+ //}
+
+//var num = 1;
+//var newNum = ++num;
+//alert(newNum)
+
+
+var where = prompt("where dooes pope live ?")
+if (where === "vatican") {
+    alert("correct");
+}
+else{
+    alert("wrong answer");
+}
+
+
+
+
+var age = prompt("How old are you?")
+if (age === "1 > 10") {
+    alert("you are a child")
+}
